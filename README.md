ce255de04d2ac5ebf66d0c1fec4640f30305ef3d
Glitch (dour-handsaw) <none>
2017-12-01 17:12:44 +0000

    null

--- README.md
+++ README.md
@@ -1,27 +1,25 @@
-Welcome to the Glitch BETA
-=========================
-
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
-
-**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
-
-Find out more [about Glitch](https://glitch.com/about).
+<!-- This is a static file -->
+<!-- served from your routes in server.js -->
 
 
-Your Project
-------------
+<!DOCTYPE html>
+<html>
+  <body>
+    <header>
+      <h1>
+        Timestamp Microservice API
+      </h1>
+    </header>
 
-On the front-end,
-- edit `public/client.js`, `public/style.css` and `views/index.html`
-- drag in `assets`, like images or music, to add them to your project
+Example usage:
 
-On the back-end,
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env` (nobody can see this but you and people you invite)
+        https://dour-handsaw.glitch.me/December%2015,%202015
+
+        https://dour-handsaw.glitch.me/1450137600
 
 
-Made by [Fog Creek](https://fogcreek.com/)
--------------------
+Example Output:
 
-\ ゜o゜)ノ
+        {"unix": 1450137600, "natural": "December 15, 2015"}
+  </body>
+</html>
--- server.js
+++ server.js
@@ -4,9 +4,22 @@
 // init project
 var express = require('express');
 var app = express();
+var date = require('date-and-time');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+app.get("/:date", function(req, res) {  
+  if (parseInt(req.params.date)) {  // check if UNIX
+    var unixConv = new Date(parseInt(req.params.date*1000));
+    res.status(200).json({"unix": req.params.date, "natural": date.format(unixConv, 'MMMM D, YYYY')}); //
+  } else if(date.isValid(req.params.date, 'MMMM D, YYYY')) { // check if written format
+    var unixConv = new Date(req.params.date);
+    res.status(200).json({"unix": parseInt(unixConv.getTime() / 1000), "natural": req.params.date});
+  } else {
+    res.status(400).end();
+  }
+});
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -15,23 +28,6 @@ app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
